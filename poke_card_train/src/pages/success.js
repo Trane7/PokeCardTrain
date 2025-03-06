7ae08c4e301dca30ed272549427271a21c1113bb
Dee <dgrowette@gmail.com>
2025-03-06 13:20:37 -0800

    set up success.js and cancel.js

---
+++ poke_card_train/src/pages/success.js
@@ -0,0 +1,8 @@
+export default function Success() {
+    return (
+      <div className="p-8">
+        <h1 className="text-3xl font-bold">Thank you for your purchase!</h1>
+        <p>Your payment was successful. You will receive an email confirmation soon.</p>
+      </div>
+    );
+  }
