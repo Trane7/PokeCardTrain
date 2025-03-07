a42ac5a62039c5fc8bc73d56574d7eebe13b7f58
Dee <dgrowette@gmail.com>
2025-03-07 12:15:20 -0800

    added logo to the homepage

--- poke_card_train/src/components/Header.js
+++ poke_card_train/src/components/Header.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';  // Import Link for navigation
 const Header = () => {
   return (
     <header>
-      <img src="/logo.png" alt="Pokémon Card Shop Logo" className="logo" />
-      <h2>Pokémon Card Shop</h2>
+      <img src="./images/pokeball.png" alt="Pokémon Card Shop Logo" className="logo" />
+      <h2>Poké Card Train</h2>
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>    {/* Home Page Link */}
