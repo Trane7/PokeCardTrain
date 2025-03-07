69aae298c85cf2ee601207c346e55b728f76ff79
Dee <dgrowette@gmail.com>
2025-03-07 12:27:12 -0800

    added mobile friendly css styles

--- poke_card_train/src/components/Home.js
+++ poke_card_train/src/components/Home.js
@@ -5,6 +5,8 @@ const Home = () => {
   return (
     <div>
       <h1>Welcome to the Pokémon Card Shop</h1>
+      <h2>How it works!</h2>
+      <h4>Grab whatever product you want to buy and add it to your cart. Head to the cart section and proceed to checkout!</h4>
       <CardList />
     </div>
   );
