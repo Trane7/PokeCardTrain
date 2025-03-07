dd532c2cf2f90c2715bb4edacd79ef84a80d4a4a
Dee <dgrowette@gmail.com>
2025-03-07 12:53:10 -0800

    added styles to navbar header and home header as well as a border around the text

--- poke_card_train/src/components/Home.js
+++ poke_card_train/src/components/Home.js
@@ -4,7 +4,7 @@ import CardList from './CardList';
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to the Pokémon Card Shop</h1>
+      <h1 className="text-with-border">Welcome to the Pokémon Card Shop</h1>
       <h2>How it works!</h2>
       <h4>Grab whatever product you want to buy and add it to your cart. Head to the cart section and proceed to checkout!</h4>
       <CardList />
